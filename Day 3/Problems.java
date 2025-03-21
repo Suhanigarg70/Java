15cf1af63d73377852f7a7c0ce22b998f3c41850
Suhani Garg <suhanigarg70@gmail.com>
2025-03-21 09:27:38 +0530

    added problems.java

---
+++ Day 3/Problems.java
@@ -0,0 +1,15 @@
+package abes.intro;
+
+public class Problems {
+	
+public static void main(String[] args) {
+		
+		int num = 92;
+		System.out.println(num<=80 && num>=20?"Present in range":"Not present in range");
+		
+		char ch = 'A';
+		System.out.println(ch>=65 && ch<=91?"Char is Upper case":"Char is lower case" );
+	}
+
+
+}
